da78af368da8047744b0bc8e21fd9d6a5abaf4bf
HiramBravo <her22034@byui.edu>
2022-10-17 13:54:49 -0500

    Change the aspect

---
+++ sentences.py
@@ -0,0 +1,24 @@
+import random
+def get_determiner(quantity):
+    """Return a randomly chosen determiner. A determiner is
+    a word like "the", "a", "one", "some", "many".
+    If quantity == 1, this function will return either "a",
+    "one", or "the". Otherwise this function will return
+    either "some", "many", or "the".
+
+    Parameter
+        quantity: an integer.
+            If quantity == 1, this function will return a
+            determiner for a single noun. Otherwise this
+            function will return a determiner for a plural
+            noun.'
+    Return: a randomly chosen determiner.
+    """
+    if quantity == 1:
+        words = ["a", "one", "the"]
+    else:
+        words = ["some", "many", "the"]
+
+    # Randomly choose and return a determiner.
+    word = random.choice(words)
+    return word
