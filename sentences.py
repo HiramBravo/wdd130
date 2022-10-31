a8929daf3f24deb91a88d10ee7a5e26c9634680c
HiramBravo <her22034@byui.edu>
2022-10-30 22:00:43 -0600

    almost

--- sentences.py
+++ sentences.py
@@ -21,4 +21,49 @@ def get_determiner(quantity):
 
     # Randomly choose and return a determiner.
     word = random.choice(words)
+    cap_word = word.capitalize()
     return word
+
+
+def get_noun(quantity):
+    """Return a randomly chosen noun.
+    If quantity == 1, this function will
+    return one of these ten single nouns:
+        "bird", "boy", "car", "cat", "child",
+        "dog", "girl", "man", "rabbit", "woman"
+    Otherwise, this function will return one of
+    these ten plural nouns:
+        "birds", "boys", "cars", "cats", "children",
+        "dogs", "girls", "men", "rabbits", "women"
+
+    Parameter
+        quantity: an integer that determines if
+            the returned noun is single or plural.
+    Return: a randomly chosen noun.
+    """
+def get_verb(quantity, tense):
+    """Return a randomly chosen verb. If tense is "past",
+    this function will return one of these ten verbs:
+        "drank", "ate", "grew", "laughed", "thought",
+        "ran", "slept", "talked", "walked", "wrote"
+    If tense is "present" and quantity is 1, this
+    function will return one of these ten verbs:
+        "drinks", "eats", "grows", "laughs", "thinks",
+        "runs", "sleeps", "talks", "walks", "writes"
+    If tense is "present" and quantity is NOT 1,
+    this function will return one of these ten verbs:
+        "drink", "eat", "grow", "laugh", "think",
+        "run", "sleep", "talk", "walk", "write"
+    If tense is "future", this function will return one of
+    these ten verbs:
+        "will drink", "will eat", "will grow", "will laugh",
+        "will think", "will run", "will sleep", "will talk",
+        "will walk", "will write"
+
+    Parameters
+        quantity: an integer that determines if the
+            returned verb is single or plural.
+        tense: a string that determines the verb conjugation,
+            either "past", "present" or "future".
+    Return: a randomly chosen verb.
+    """
